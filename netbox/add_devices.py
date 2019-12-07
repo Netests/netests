5813aefdd6ad51e1875ff88fc11702a0f559a0a6
Dylan-Hamel <contact@dylan-hamel.ch>
2019-12-07 11:47:03 +0100

    Clean local repo

---
+++ netbox/add_devices.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
+
+"""
+Add a description ....
+
+"""
+
+__author__ = "Dylan Hamel"
+__maintainer__ = "Dylan Hamel"
+__version__ = "1.0"
+__email__ = "dylan.hamel@protonmail.com"
+__status__ = "Prototype"
+__copyright__ = "Copyright 2019"
+
+########################################################################################################################
+#
+# HEADERS
+#
+ERROR_HEADER = "Error import [bgp_checks.py]"
+HEADER_GET = "[netests - bgp_checks]"
+
+########################################################################################################################
+#
+# Import Library
+#
+try:
+    from const.constants import *
+except ImportError as importError:
+    print(f"{ERROR_HEADER} const.constants")
+    exit(EXIT_FAILURE)
+    print(importError)
+
+try:
+    from const.netbox_const import *
+except ImportError as importError:
+    print(f"{ERROR_HEADER} const.netbox_const")
+    exit(EXIT_FAILURE)
+    print(importError)
+
+try:
+    from nornir.core import Nornir
+    # To use advanced filters
+    from nornir.core.filter import F
+    # To execute netmiko commands
+    from nornir.plugins.tasks.networking import netmiko_send_command
+    # To execute napalm get config
+    from nornir.plugins.tasks.networking import napalm_get
+    # To print task results
+    from nornir.plugins.functions.text import print_result
+except ImportError as importError:
+    print(f"{ERROR_HEADER} nornir")
+    print(importError)
+    exit(EXIT_FAILURE)
+
+########################################################################################################################
+#
+# Functions
+#
+def add_devices_in_netbox(nr):
+
+    devices = nr.filter()
+
+    if len(devices.inventory.hosts) == 0:
+        raise Exception(f"[{HEADER_GET}] no device selected.")
+
+    data = devices.run(
+        task=_generic_add_interface,
+        on_failed=True,
+        num_workers=10
+    )
+    print_result(data)
+
+def _generic_add_interface(task):
+
+    data = task.run(
+        task=napalm_get,
+        getters=["interfaces"]
+    )
+    interfaces = data.result["interfaces"]
+    print(interfaces)
